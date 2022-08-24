a60334ac04381b13e2b6eeab72c30b98c4c0f268
Caroline Thostrup <caro342h@stud.kea.dk>
2022-08-24 14:05:41 +0200

    added basic js structure

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", init);
+
+const database = "actors.json";
+
+// Fetch Data
+fetch(database)
+  .then((res) => res.json())
+  .then((data) => handleActors(data));
+
+function handleActors(actors) {
+  console.log(actors);
+}
+
+// Populate page
+
+// Grab template
+
+// Clone template
+
+// Change Content
+
+// Grab Parent
+
+// Append Parent
+
+// Show info card
